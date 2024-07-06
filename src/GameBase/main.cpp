0a8d2c9332747028a0b3a41d0b342cc8fabf201f
Nadeen Haj yahia <nadeenha@std.hac.ac.il>
2024-07-06 03:02:48 +0300

    corrected the sharp and witch photo

--- src/GameBase/main.cpp
+++ src/GameBase/main.cpp
@@ -3,6 +3,33 @@
 #include "Singleton.h"
 #include <SFML/Graphics.hpp>
 
+//int main() {
+//    sf::RenderWindow window(sf::VideoMode(800, 600), "Witch Image");
+//    sf::Texture texture;
+//
+//    if (!texture.loadFromFile("sharp.png")) {
+//        return -1; // Error loading image
+//    }
+//
+//    sf::Sprite sprite;
+//    sprite.setTexture(texture);
+//
+//    while (window.isOpen()) {
+//        sf::Event event;
+//        while (window.pollEvent(event)) {
+//            if (event.type == sf::Event::Closed)
+//                window.close();
+//        }
+//
+//        window.clear();
+//        window.draw(sprite);
+//        window.display();
+//    }
+//
+//    return 0;
+//}
+
+
 int main() {
     //logic mood
     /*sf::RenderWindow m_window;
